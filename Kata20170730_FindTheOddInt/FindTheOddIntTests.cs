a2f59c43de61c63d4020ebfe9f8a43d7581a143f
cash <cash@cashwu.com>
2017-07-30 19:40:29 +0800

    input  20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5 test case

--- Kata20170730_FindTheOddInt/FindTheOddIntTests.cs
+++ Kata20170730_FindTheOddInt/FindTheOddIntTests.cs
@@ -19,6 +19,12 @@ namespace Kata20170730_FindTheOddInt
             AssertFindOddShouldBe(new[] { 2, 2, 1 }, 1);
         }
 
+        [TestMethod]
+        public void inputn_20_1_n1_2_n2_3_3_5_5_1_2_4_20_4_n1_n2_5_should_return_5()
+        {
+            AssertFindOddShouldBe(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }, 5);
+        }
+
         private static void AssertFindOddShouldBe(int[] seq, int expected)
         {
             var kata = new Kata();
