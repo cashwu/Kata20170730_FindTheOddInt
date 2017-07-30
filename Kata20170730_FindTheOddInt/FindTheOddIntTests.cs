f7a919d5d5dda1b18efb57b921bad54b1eda45f2
cash <cash@cashwu.com>
2017-07-30 19:31:24 +0800

    seq 2 , 2 test case

--- Kata20170730_FindTheOddInt/FindTheOddIntTests.cs
+++ Kata20170730_FindTheOddInt/FindTheOddIntTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170730_FindTheOddInt
     public class FindTheOddIntTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_2_2_should_return_0()
         {
+            AssertFindOddShouldBe(new[] {2, 2}, 0);
+        }
+
+        private static void AssertFindOddShouldBe(int[] seq, int expected)
+        {
+            var kata = new Kata();
+            var actual = kata.find_it(seq);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int find_it(int[] seq)
+        {
+            return 0;
         }
     }
 }
